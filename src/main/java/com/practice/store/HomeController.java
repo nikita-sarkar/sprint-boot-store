057f3f4f1087da5dba278b91f833e477a387333f
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-04-20 19:50:55 +0530

    Configuring and using application properties

--- src/main/java/com/practice/store/HomeController.java
+++ src/main/java/com/practice/store/HomeController.java
@@ -1,13 +1,20 @@
 package com.practice.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("${app.page-size}")
+    private String appSize;
+
     @RequestMapping("/")
     public String index()
     {
+        System.out.print("appSize: " + appSize);
+
         return "index.html";
     }
 }
